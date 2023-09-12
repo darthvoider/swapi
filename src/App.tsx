054d1364d01a631a05ee4ba3f06fa91e55ffba04
darthvoider <voiderok@gmail.com>
2023-09-12 19:20:08 +0300

    add Router stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ReactQueryProvider } from "./providers";
 import { PeopleListing } from "./views";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PeopleListing />,
+  },
+]);
 
 export default function App() {
   return (
     <ReactQueryProvider>
-      <PeopleListing />
+      <RouterProvider router={router} />
     </ReactQueryProvider>
   );
 }
