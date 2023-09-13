47b5109e02b14dc8164ca410eaf8ea013fa97bbe
darthvoider <voiderok@gmail.com>
2023-09-13 20:48:03 +0300

    refactor API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,25 +1,26 @@
 import axios from "axios";
 import humps, { Camelized } from "humps";
-import { IPeopleData } from "../types";
+import { IPeopleData, ICharacter, ISpecie } from "../types";
 
 const baseURL = "https://swapi.dev/api/";
 
 export const axiosClient = axios.create({ baseURL });
 
+const axiosGet = (url: string) =>
+  axiosClient.get(url).then((res) => humps.camelizeKeys(res.data));
+
 export const fetchPeople = (
   page: number
-): Promise<IPeopleData | Camelized<any>> =>
-  axiosClient
-    .get(`/people/?page=${page}`)
-    .then((res) => humps.camelizeKeys(res.data));
+): Promise<IPeopleData | Camelized<any>> => axiosGet(`/people/?page=${page}`);
 
 export const searchPeople = (
   searchParam: string,
   page: number
-): Promise<IPeopleData | Camelized<any>> =>
-  axiosClient
-    .get(`/people/?search=${searchParam}&page=${page}`)
-    .then((res) => humps.camelizeKeys(res.data));
+): Promise<Camelized<IPeopleData>> =>
+  axiosGet(`/people/?search=${searchParam}&page=${page}`);
 
-export const fetchProfile = (id?: string) =>
-  axiosClient.get(`/people/${id}`).then((res) => humps.camelizeKeys(res.data));
+export const fetchProfile = (id?: string): Promise<Camelized<ICharacter>> =>
+  axiosGet(`/people/${id}`);
+
+export const fetchSpecies = (id?: string): Promise<Camelized<ISpecie>> =>
+  axiosGet(`/species/${id}`);
